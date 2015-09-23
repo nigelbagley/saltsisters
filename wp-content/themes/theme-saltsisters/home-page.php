9e4661e6a49ef5a960f5e615a22c69cb772f0748
nigelbagley <bagley.nigel@gmail.com>
2015-09-22 20:15:11 -0400

    updated images

--- wp-content/themes/theme-saltsisters/home-page.php
+++ wp-content/themes/theme-saltsisters/home-page.php
@@ -35,10 +35,62 @@ get_header();  ?>
 	</div>
 	<div class="filler-image"></div>
 	<div class="container">
-	<div class="about-section container-margin">
-		<h1>About Us</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores culpa, aut eaque et suscipit reiciendis. Nisi quo nobis quam quos sit quasi minus dolorem voluptas ipsum, repellat laborum, dolor, voluptatibus itaque? Ipsum, accusamus sint atque, doloremque reprehenderit ratione alias, ut repellendus perferendis id eius. Explicabo laboriosam nesciunt, omnis molestiae et optio, accusamus soluta nemo, illum atque, aliquid tenetur natus. Voluptatibus eligendi ratione ipsa, expedita fugiat aliquid consectetur. Soluta nisi, tempora quia voluptatem et eaque adipisci odio consectetur, praesentium tempore ex!</p>
+		<div class="about-section container-margin">
+			<h1>About Us</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores culpa, aut eaque et suscipit reiciendis. Nisi quo nobis quam quos sit quasi minus dolorem voluptas ipsum, repellat laborum, dolor, voluptatibus itaque? Ipsum, accusamus sint atque, doloremque reprehenderit ratione alias, ut repellendus perferendis id eius. Explicabo laboriosam nesciunt, omnis molestiae et optio, accusamus soluta nemo, illum atque, aliquid tenetur natus. Voluptatibus eligendi ratione ipsa, expedita fugiat aliquid consectetur. Soluta nisi, tempora quia voluptatem et eaque adipisci odio consectetur, praesentium tempore ex!</p>
+			<a href="#">Read More</a>
+		</div>
+		<div class="dates-rates">
+			<h1>Retreat Details</h1>
+			<div class="dates-rates-container">
+				<div class="dates-rates-image-winter box">
+					<div class="dates-rates-content">
+						<div class="content-title">
+							<h3>Accomodations</h3>
+							<!-- <h4>$$$$</h4> -->
+						</div>
+						<div class="days">
+							<!-- <h4>8 Days</h4> -->
+						</div>
+					</div>
+				</div>
+				<div class="dates-rates-image-spring box">
+					<div class="dates-rates-content">
+						<div class="content-title">
+							<h3>Lessons</h3>
+							<!-- <h4>$$$$</h4> -->
+						</div>
+						<div class="days">
+							<!-- <h4>8 Days</h4>-->
+						</div>
+					</div>
+				</div>
+				<div class="dates-rates-image-summer box">
+					<div class="dates-rates-content">
+						<div class="content-title">
+							<h3>Activities</h3>
+							<!-- <h4>$$$$</h4> -->
+						</div>
+						<div class="days">
+							<!-- <h4>8 Days</h4> -->
+						</div>
+					</div>
+				</div>
+				<div class="dates-rates-image-fall box">
+					<div class="dates-rates-content">
+						<div class="content-title">
+							<h3>Menu</h3>
+							<!-- <h4>$$$$</h4> -->
+						</div>
+						<div class="days">
+							<!-- <h4>8 Days</h4> -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
+
     <?php // Start the loop ?>
     <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
@@ -46,7 +98,7 @@ get_header();  ?>
       <?php the_content(); ?>
 
     <?php endwhile; // end the loop?>
-  </div> <!--/.containter -->
+  <!-- </div> --> <!--/.containter -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/theme-saltsisters/winter-page.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+	Template Name: Winter Page
+*/
+
+get_header();  ?>
+
+<div class="main">
+
+  <div class="winter-page-hero">
+  	<div class="dates">
+  		<div class="date">
+  			<h3>Date</h3>
+  			<h4>DEC - DEC</h4>
+  		</div>
+  		<div class="days">
+  			<h3>Days</h3>
+  			<h4>8</h4>
+  		</div>
+  		<div class="price">
+  			<h3>Price</h3>
+  			<h4>$$$$</h4>
+  		</div>
+  		<div class="availabilty">
+  			<h3>Availability</h3>
+  			<h4>1</h4>
+  		</div>
+  		<div class="book-now">
+  			<a href="#">Book Now</a>
+  		</div>	
+  	</div>
+  </div>
+
+	<div class="container">
+		<div class="dates-container">
+			<h1>Lorem ipsum dolor sit amet.</h1>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga mollitia laborum possimus ullam eaque architecto consequatur saepe voluptates et dolorum, eveniet amet enim omnis, consequuntur vero autem, dolores debitis beatae!</p>
+			<iframe src="https://player.vimeo.com/video/76984499" width="700" height="394" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		</div>
+
+	</div>
+	<div class="filler-image"></div>
+	<div class="container">
+	<h1>Lorem ipsum dolor sit.</h1>
+	<!-- <p>Lorem ipsum dolor sit amet, consectetur.</p> -->
+		<div class="day-content">
+			<div class="day-containers">
+				<h3>Days 1-2</h3>
+				<h2>Lorem ipsum dolor.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum aliquid omnis odio numquam cupiditate quasi laboriosam eligendi quas neque itaque.</p>
+			</div>
+			<div class="day-containers">
+				<h3>Days 3-4</h3>
+				<h2>Lorem ipsum dolor.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum aliquid omnis odio numquam cupiditate quasi laboriosam eligendi quas neque itaque.</p>
+			</div>
+			<div class="day-containers">
+				<h3>Days 5-6</h3>
+				<h2>Lorem ipsum dolor.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum aliquid omnis odio numquam cupiditate quasi laboriosam eligendi quas neque itaque.</p>
+			</div>
+			<div class="day-containers">
+				<h3>Days 7-8</h3>
+				<h2>Lorem ipsum dolor.</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum aliquid omnis odio numquam cupiditate quasi laboriosam eligendi quas neque itaque.</p>
+			</div>
+		</div>
+	</div>
+
+
+    <?php // Start the loop ?>
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+      <!-- <h2><?php the_title(); ?></h2> -->
+      <?php the_content(); ?>
+
+    <?php endwhile; // end the loop?>
+  <!-- </div> --> <!--/.containter -->
+</div> <!-- /.main -->
+
+<?php get_footer(); ?>
